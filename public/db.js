17b63830ed20cac0548bc3966085f4e8d173a3a7
Steven Woo <swoo9875@gmail.com>
2021-04-24 19:03:37 -0400

    testing

--- public/db.js
+++ public/db.js
@@ -1,10 +1,3 @@
-const indexedDB =
-  window.indexedDB ||
-  window.mozIndexedDB ||
-  window.webkitIndexedDB ||
-  window.msIndexedDB ||
-  window.shimIndexedDB;
-
 let db;
 // create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
