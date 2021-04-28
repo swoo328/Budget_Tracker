36e039b8a7c190f2748a59cffb3df93c14791da8
Steven Woo <swoo9875@gmail.com>
2021-04-27 20:37:00 -0400

    deploying

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 8080;
+const PORT = 3000;
 
 const app = express();
 
